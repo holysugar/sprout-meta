8ed8dec850584e63927e1221b763995ea3165e24
holysugar <holysugar@gmail.com>
2013-07-22 13:55:18 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+sprout-meta
+===========
+
+See [sprout-wrap](https://github.com/pivotal-sprout/sprout-wrap).  This repository has same feature.
+I write this to customize sprout-wrap in zero-base.
+
---
+++ site-cookbooks/meta/metadata.rb
@@ -0,0 +1,12 @@
+maintainer        'holysugar'
+maintainer_email  'holysugar@gmail.com'
+license           'MIT'
+description       'holysugars osx install'
+long_description  'holysugars osx install'
+version           '0.1.0'
+supports          'mac_os_x'
+
+depends           'pivotal_workstation'
+depends           'sprout-osx-settings'
+depends           'sprout-osx-apps'
+depends		  'sprout-holysugar'
